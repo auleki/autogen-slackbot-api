bf68a0fc463a49ab951ab768fb34eea25ed3ff0e
Emmanuel B. Giwa <iamtundegiwa@gmail.com>
2023-11-03 20:39:21 +0100

    include requirement file

--- autogen_test.py
+++ autogen_test.py
@@ -12,13 +12,21 @@ assistant = autogen.AssistantAgent(
 
 user_proxy = autogen.UserProxyAgent(
     name="user_proxy",
-    human_input_mode="ALWAYS",
-    code_execution_config={"work_dir": "_output", "use_docker": "python3" },
+    human_input_mode="TERMINATE",
+    code_execution_config={"work_dir":"_output", "use_docker":"python:3" },
     is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE")
 )
 
-#to initiate conversation with user
-user_proxy.initiate_chat(assistant, message="Besides space rockets, how can I get to the moon?")
+dump_command = " and store the output the final result into a text file called result.txt"
+
+def talk_to_autogen():
+    #to initiate conversation with user
+    user_proxy.initiate_chat(assistant, message="Define what an equator is")
+    msg = user_proxy.generate_reply(assistant)
+    print("Last message is:")
+    print(msg)
+
+talk_to_autogen()
 
 #to enable logging
 autogen.ChatCompletion.start_logging()
