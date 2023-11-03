615571ad447333bcfada72ed8d129f4e28543522
Emmanuel B. Giwa <iamtundegiwa@gmail.com>
2023-11-03 21:06:15 +0100

    change package version and python version

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3.8
 from fastapi import FastAPI
 app = FastAPI()
 
@@ -5,5 +6,8 @@ app = FastAPI()
 async def home():
     return {"Hi Cold World"}
 
+@app.post("/discord-message")
+async def receive_msg():
+    return { "Send Messgage to AI" }
 # add endpoint to receive messages from slack webhook
 # on receiving message from slack, feed that into AutoGen
